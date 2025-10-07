98dddfde7ec10613a31de611d8df358321296cd1
Siavash Davani <siavash.davani@gmail.com>
2025-10-07 09:59:51 +0000

    populated the simulator preparation code

---
+++ packages/qiskit-aer-encore/src/qiskit_aer_encore/simulator.py
@@ -0,0 +1,29 @@
+"""Module for Qiskit simulators."""
+from qiskit_aer import AerSimulator, AerError
+
+def generate_aer_simulator(force_gpu: bool = False) -> AerSimulator:
+    """A function to prepare the Aer simulator with desired configuration. GPU accelerated in case available.
+    Args:
+        force_gpu (bool): If True, enforces GPU usage which means it will throw an error if no GPU is available. Default is False.
+    Returns:
+        AerSimulator: The prepared Aer simulator.
+    """
+    available_devices: tuple[str] = AerSimulator().available_devices()  # type: ignore[]
+    if "GPU" in available_devices:
+        simulator = AerSimulator(
+            device="GPU",
+            # enable accelerating using Nvidia's cuStateVec library
+            cuStateVec_enable=True,
+            # maximize the use of all available GPU and CPU nodes
+            blocking_enable=True,
+            # distribute shots to different available GPUs
+            batched_shots_gpu=True,
+        )
+    elif force_gpu:
+        raise AerError("Asked for GPU-accelerated simulation but no GPU was available.")
+    else:
+        simulator = AerSimulator(
+            device="CPU",
+        )
+
+    return simulator
