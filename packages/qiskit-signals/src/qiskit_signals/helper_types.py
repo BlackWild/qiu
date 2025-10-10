324b163f109d170e272a05f93dcdd18f2fd8693f
Siavash Davani <siavash.davani@gmail.com>
2025-10-10 15:54:41 +0000

    removed unused to fix circular import bug

--- packages/qiskit-signals/src/qiskit_signals/helper_types.py
+++ packages/qiskit-signals/src/qiskit_signals/helper_types.py
@@ -6,12 +6,8 @@ from typing import Literal
 import numpy as np
 import numpy.typing as npt
 
-from qiskit_signals.quantum_axis import GenericAxis
-
 SignalFunctionType = Callable[[np.ndarray], np.ndarray]
 RealSignalFunctionType = Callable[[np.ndarray], npt.NDArray[np.float64]]
 ComplexSignalFunctionType = Callable[[np.ndarray], npt.NDArray[np.complex128]]
-
-AxisFunctionType = Callable[[GenericAxis], np.ndarray]
 
 EncodingType = Literal["twos_complement", "unsigned"]
